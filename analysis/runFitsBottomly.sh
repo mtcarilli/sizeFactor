a2cd116b482047dce12066dbc3ffa1833390487c
Maria Carilli <miacarilli@gmail.com>
2025-04-08 16:44:27 -0700

    continuing bottomly analysis

---
+++ analysis/runFitsBottomly.sh
@@ -0,0 +1,35 @@
+
+readarray -t count_path_list <<EOF
+nonzero_gene_counts_25p_.5f.txt  
+nonzero_gene_counts_75p_.5f.txt
+nonzero_gene_counts_25p.txt      
+nonzero_gene_counts_75p.txt
+nonzero_gene_counts_10p_.5f.txt  
+nonzero_gene_counts_50p_.5f.txt  
+nonzero_gene_counts_90p_.5f.txt
+nonzero_gene_counts_10p.txt      
+nonzero_gene_counts_50p.txt      
+nonzero_gene_counts.txt
+EOF
+
+
+readarray -t results_path_list <<EOF
+nonzero_gene_counts_25p_.5f
+nonzero_gene_counts_75p_.5f
+nonzero_gene_counts_25p  
+nonzero_gene_counts_75p
+nonzero_gene_counts_10p_.5f 
+nonzero_gene_counts_50p_.5f 
+nonzero_gene_counts_90p_.5f
+nonzero_gene_counts_10p     
+nonzero_gene_counts_50p    
+nonzero_gene_counts
+EOF
+
+for i in "${!count_path_list[@]}"; do
+    count_path="${count_path_list[$i]}"
+    results_path="${results_path_list[$i]}"
+    echo $count_path
+    Rscript runDESeq2EdgeR.R --count_path $count_path --results_path $results_path --reps1 10 --reps2 11
+    python runGLMfit.py --count_path $count_path --results_path $results_path
+done
---
+++ analysis/runGLMfit.py
@@ -0,0 +1,54 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--count_path")
+parser.add_argument("--results_path")
+parser.add_argument("--ridge_factor",default=1e-6,type=np.float)
+args = parser.parse_args()
+count_path = args.count_path
+results_path = args.results_path
+
+
+import numpy as np
+import pandas as pd
+import scipy
+
+import glmFit
+
+
+min_mu = 0.5
+beta_tol = 1e-8
+min_bat = -30
+max_beta = 30
+maxiter = 250
+optimizer = "L-BFGS-B"
+
+
+# counts
+counts = pd.read_csv(count_path,sep="\t",header=None)
+
+R_list = [10,11]
+S = 21
+C = 2
+G = counts.shape[0]-1 # genes
+
+design = glmFit.getDesignExtended(C,R_list,G)
+
+dispersions = pd.read_csv(f'{results_path}_deseq2_dispersions.csv').dispGeneEst.values # per gene 
+disp = np.repeat(dispersions,S)
+
+
+counts_flattened = counts.values.T.flatten()   # need the format to be GENE BY SAMPLE
+X = design
+
+# do the fitting
+beta, mu, converged  =  glmFit.irls_solver(
+                                counts = counts_flattened,
+                                design_matrix = design,
+                                disp = disp,
+                                )
+
+
+
+# save beta and mu 
+np.savetxt(f'{results_path}_glmFit_weights.txt',beta, delimiter='\t')
+np.savetxt(f'{results_path}_glmFit_mu.txt',mu, delimiter='\t')
